44748f6d83363c3061ee6017f45e941e16341f2e
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 22:02:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,47 +1,41 @@
-require 'pry'
-def dictionary
-  dictionary =  {
-    "hello" => "hi",
-    "to" => "2", "two" => "2", "too" => "2",
-    "for" => "4", 
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
-  }
-end
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  short_tweet = []
 
-def word_substituter(string)
- new_word = string.split
- keys = dictionary.keys
-   new_word.map! do |word|
-      if keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else 
-        word = word
+  words.each do |word|
+    case word.downcase
+    when "hello"
+      short_tweet << "hi"
+    when "to", "two", "too"
+      short_tweet << "2"
+    when "for", "four" 
+      short_tweet << "4"
+    when "be" 
+      short_tweet << "b"
+    when "you" 
+      short_tweet << "u"
+    when "at" 
+      short_tweet << "@" 
+    when "and" 
+      short_tweet << "&"
+    else
+      short_tweet << word
     end
   end
-  new_word.join(" ")
+  short_tweet.join(" ")
 end
-
-
 
 def bulk_tweet_shortener(tweets)
-  
   tweets.each do |tweet|
-    puts word_substituter(tweet)
-  end
+    tweet = word_substituter(tweet)
+    puts "#{tweet}"
+  end 
 end
 
-
 def selective_tweet_shortener(tweet)
-   if tweet.to_s.length > 140 
-    puts word_substituter(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
   else
-    puts tweet
+    tweet
   end
-end 
-
-
-
+end
