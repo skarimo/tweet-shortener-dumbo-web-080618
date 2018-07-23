b8f546cd9729bef02e7a2a673f3b931849c62afe
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 22:11:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,10 +44,11 @@ def selective_tweet_shortener(tweet)
 end 
 
 def shortened_tweet_truncator(tweet)
-  if tweet.length > 140 
-    word_substituter(tweet)
+ s_tweet = word_substituter(tweet)
+  if s_tweet.length > 140 
+    tweet[0..139] + "..."
   else
-    word_substituter[tweet[0..136]] + "..."
+    tweet
   end
 end
   
